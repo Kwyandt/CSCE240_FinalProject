7f15fa255eb90ea9fe101c8913ec55219b3a33b4
Katelyn Wyandt <kat.wyandt.812@gmail.com>
2022-12-05 19:39:50 -0500

    年后

--- Assignment/LList.cpp
+++ Assignment/LList.cpp
@@ -2,3 +2,67 @@
 #include "LList.h"
 
 using namespace std;
+
+/* friend ostream & operator<< (ostream &lhs, const LList &rhs);
+friend istream & operator>> (istream &lhs, LList &rhs); */
+LList::LList(){
+
+}
+LList::LList(int[], int size){
+
+}
+LList::LList(const LList&){
+
+}
+        
+void LList::insert (int index, int value){
+
+}
+int LList::remove(int index){
+
+}
+bool LList::contains(int value){
+
+}
+int LList::indexOf(int value){
+
+}
+bool LList:: isEmpty(){
+
+}
+int LList::size(){
+
+}
+void LList::clear(){
+
+}
+
+const LList LList::operator+(const LList &rhs)const{
+
+}
+const LList LList::operator=(const LList &rhs){
+
+}
+int LList::operator[](int index) const{
+
+}
+int LList:: & operator[](int index){
+
+}
+bool LList::operator==(const LList &rhs) const{
+
+}
+        
+void LList::setData(int data, int index){
+
+}
+Datum LList::getData(int index) const{
+
+}
+int LList::getSize() const{
+
+}
+
+void LList::setSize(int size){
+
+}
