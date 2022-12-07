3cf679048188fd83cff14c5a5b7323ec647e8be6
calebhenry <calebhenry2003@gmail.com>
2022-12-07 15:51:07 -0500

    hi

--- Assignment/LList.cpp
+++ Assignment/LList.cpp
@@ -4,10 +4,21 @@
 
 using namespace std;
 
+/**
+ * @brief Construct a new LList::LList object
+ * 
+ */
 LList::LList(){
     setSize(0);
     head = tail = NULL;
 }
+
+/**
+ * @brief Construct a new LList::LList object
+ * 
+ * @param arr An array that the LList is created out of
+ * @param size The size of the array
+ */
 LList::LList(int arr[], int size){
     head = tail = NULL;
     setSize(0);
@@ -15,6 +26,12 @@ LList::LList(int arr[], int size){
         insert(i, arr[i]);
     }
 }
+
+/**
+ * @brief Construct a copy of a LList::LList object
+ * 
+ * @param list The list that is being copied
+ */
 LList::LList(const LList& list){
     head = tail = NULL;
     setSize(0);
@@ -23,7 +40,10 @@ LList::LList(const LList& list){
     }
 }
 
-//destructor
+/**
+ * @brief Destroy the LList::LList object
+ * 
+ */
 LList::~LList() {
     //length = 0;
     Datum *temp = head;
@@ -34,6 +54,12 @@ LList::~LList() {
     }
 }
         
+/**
+ * @brief Inserts a new node at the supplied index
+ * 
+ * @param index The index the node gets added at
+ * @param value The value of the new node
+ */
 void LList::insert (int index, int value){
     if(index <= 0){
         Datum *temp = head;
@@ -63,6 +89,13 @@ void LList::insert (int index, int value){
         length++;
     }
 }
+
+/**
+ * @brief Removes the node from that index
+ * 
+ * @param index The index at that location
+ * @return int The value at that index
+ */
 int LList::remove(int index){
     if(size() == 0){
         return -1;
@@ -100,6 +133,14 @@ int LList::remove(int index){
     }
     return ret;
 }
+
+/**
+ * @brief Checks if the list contains a certian value
+ * 
+ * @param value The value that is being checked for 
+ * @return true If the list contains the value
+ * @return false If the list does not contain the value
+ */
 bool LList::contains(int value){
     Datum *temp = head;
     for(int i = 0; i < size(); i++){
@@ -110,6 +151,13 @@ bool LList::contains(int value){
     }
     return false;
 }
+
+/**
+ * @brief Finds the index of a certian value
+ * 
+ * @param value The value that is being searched for
+ * @return int The index of that value
+ */
 int LList::indexOf(int value){
     Datum *temp = head;
     int index = 0;
@@ -122,15 +170,33 @@ int LList::indexOf(int value){
     }
     return -1;
 }
+
+/**
+ * @brief Checks if the linked list is empty
+ * 
+ * @return true Returns true if the list is empty
+ * @return false Returns false if the list has stuff in it
+ */
 bool LList:: isEmpty(){
     if(head==NULL){
         return true;
     }
     return false;
 }
+
+/**
+ * @brief Returns the size of the list
+ * 
+ * @return int The size of the list
+ */
 int LList::size() const{
     return length;
 }
+
+/**
+ * @brief Emptys the list out
+ * 
+ */
 void LList::clear(){
     length = 0;
     Datum *temp = head;
@@ -141,6 +207,12 @@ void LList::clear(){
     }
 }
 
+/**
+ * @brief Adds the supplied list to the end of the current one
+ * 
+ * @param rhs The list thats being added
+ * @return const LList The combination of the list
+ */
 const LList LList::operator+(const LList &rhs)const{
     if(size()>0 && rhs.size()>0){
         LList newList(*this);
@@ -153,8 +225,14 @@ const LList LList::operator+(const LList &rhs)const{
         return rhs;
     }
     return *this;
-
 }
+
+/**
+ * @brief Sets the current list to the supplied list
+ * 
+ * @param rhs 
+ * @return const LList 
+ */
 const LList LList::operator=(const LList &rhs){
     clear();
     for(int i=0; i<rhs.size(); i++){
@@ -236,40 +314,18 @@ int LList::getData(int index) const{
     
 }
 
-/**
- * @brief setter for length variable
- * 
- * @param _size interger value for size
- */
 void LList::setSize(int _size){
     length = _size;
 }
 
-/**
- * @brief getter for head variable
- * 
- * @return head pointer
- */
 Datum LList::getHead() const {
     return *head;
 }
 
-/**
- * @brief getter for tail vairable
- * 
- * @return tail pointer 
- */
 Datum LList::getTail() const {
     return *tail;
 }
 
-/**
- * @brief output stream  method
- * 
- * @param lhs output stream
- * @param rhs datum being output
- * @return output stream
- */
 ostream& operator<<(ostream &lhs, const LList &rhs) {
     if(rhs.size() == 0)
         lhs << "-";
@@ -278,15 +334,11 @@ ostream& operator<<(ostream &lhs, const LList &rhs) {
     }
     return lhs;
 }
-
-/**
- * @brief input overload method
- * 
- * @param lhs input stream 
- * @param rhs datum being appended to
- * @return input stream
- */
+//obtain a value of stream to be appended to the end of the LList object
+//TODO not actually modiying
 istream& operator>>(istream &lhs, LList &rhs) {
+    //lhs >> rhs[rhs.size()];
+    /* rhs.length = rhs.length + 1; */
     int input;
     lhs >> input;
     rhs.insert(rhs.size(), input);
